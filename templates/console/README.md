2c8c2c9800b7dbb3b93b335061b344a32246e16d
Alejandro Hernandez <alejandrohs@gmail.com>
2017-06-21 17:51:21 +0200

    Added optional argument to the console template

--- templates/console/README.md
+++ templates/console/README.md
@@ -1,5 +1,5 @@
 Skeleton of a command line application with a single command
 
-Run `composer install` execute your command running `php command`
+Run `composer install` execute your command running `php command` or `php command me`
 
-The code in the `execute` method of `src/Command.php` will be run when the command is executed
+The code in the `execute` method of `src/Command.php` will be executed when the command is run
--- templates/console/src/Command.php
+++ templates/console/src/Command.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Console\Command\Command as CommandBase;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -8,12 +9,17 @@ class Command extends CommandBase
 {
     protected function configure()
     {
-        $this->setName('command');
+        $this
+            ->setName('command')
+            ->setDescription('Example of command with one parameter')
+            ->addArgument('name', InputArgument::OPTIONAL, 'User to greet', 'World');
     }
  
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Hello world!');
+        $name = $input->getArgument('name');
+
+        $output->writeln("Hello {$name}!");
     }
 }
 
